ac24cfa8b478e9dc2587da1323936ed0f4430ece
NatsuFuji <nathanieldojillo007@gmail.com>
2025-03-22 14:18:52 +0800

    Responsive Quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DisplayquotesComponent } from './displayquotes/displayquotes.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ DisplayquotesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'its320frontend-dojillo';
+  title = 'ITS320_FRONTEND_DOJILLO';
 }
